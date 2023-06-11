cd61e1824da58e1d2e6735cfd1cc0562792b0694
Katharina <katharinazlosa@gmail.com>
2023-06-11 21:41:58 +0200

    modal added

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,22 @@
+import { MouseEventHandler, useState } from "react";
+
 type ButtonProps = {
   text: string;
   color?: "red" | "green" | "blue";
+  onClick?: MouseEventHandler;
 };
 
-const Button = ({ text, color = "green" }: ButtonProps) => {
+const Button = ({ onClick, text, color = "green" }: ButtonProps) => {
+  const [modal, setModal] = useState<boolean>(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
   return (
     <>
-      <button className={`btn btn--${color}`}>{text}</button>
+      <button className={`btn btn--${color}`} onClick={toggleModal}>
+        {text}
+      </button>
     </>
   );
 };
