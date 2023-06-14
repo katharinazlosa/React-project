c286ebb68f4f85c0a790dd5bf4bac4e17b6f00bf
grafika10 <grafika10@ucione.local>
2023-06-14 21:42:43 +0200

    new component added

---
+++ src/assets/icons/iconmenu.tsx
@@ -0,0 +1,35 @@
+const IconMenu = () => {
+  return (
+    <>
+      <svg
+        fill="none"
+        height="24"
+        stroke-width="1.5"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3 5H21"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M3 12H21"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M3 19H21"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default IconMenu;
